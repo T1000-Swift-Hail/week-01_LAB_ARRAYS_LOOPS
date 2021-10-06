d065ef1f249e00febb41276067ea5f5acc882f44
reem abdullah <reema_33@MacBook-Air-reem.local>
2021-10-06 10:13:42 +0300

    Done

---
+++   loops.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var cities = ["Hail", "Riyadh" , "Khobar" , "Dammam" ,"Taif"]
+cities.insert("jeddah", at:3)
+for city  in cities {
+    print ("I visited \(city) ")
+    
+}
