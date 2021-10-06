aea880cf31054b48bd95b311fd2f76289ec98c90
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-06 14:32:08 +0300

    LAB_ARRAYS_LOOPS

--- week-01_LAB_ARRAYS_LOOPS.playground/Contents.swift
+++ week-01_LAB_ARRAYS_LOOPS.playground/Contents.swift
@@ -2,23 +2,25 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-var cities = ["USA","Dubai"]
+//var cities = ["USA","Dubai"]
+//
+//for visitedCities in cities {
+//    print("I visited \(visitedCities)")
+//}
+//
+//cities.insert("France", at: 2)
+//print(cities)
+//
+//for (index, cities) in cities .enumerated(){
+//
+//    print("\(index): \(cities)")
+//}
+//
+//cities.insert("Colorado", at: 3)
+//
+//for visitedCities in cities {
+//    print("I visited \(visitedCities)")
+//}
 
-for visitedCities in cities {
-    print("I visited \(visitedCities)")
-}
 
-cities.insert("France", at: 2)
-print(cities)
-
-for (index, cities) in cities .enumerated(){
-    
-    print("\(index): \(cities)")
-}
-
-cities.insert("Colorado", at: 3)
-
-for visitedCities in cities {
-    print("I visited \(visitedCities)")
-}
-
+for i in odd
