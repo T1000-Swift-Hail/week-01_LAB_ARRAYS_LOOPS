235e8eba4dc97cf5f50cf77e41be9198ca6c75b6
mac <mac@MacBook-Pro-alkhas-b-mac.local>
2021-10-06 11:24:25 +0300

    Done Lab

---
+++ week-01_LAB_ARRAYS_LOOPS.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var cities = ["Hail" , "Makah" , "Riyedh" , "Algoaf " , "Almadeina"]
+
+var visitedCities = [ "Makah", "Riyedh" , "Gadah"]
+
+visitedCities += ["Alqsaem" ,  "Ckaka" ]
+
+for visited in visitedCities {
+
+ 
+print(visited)
+}
