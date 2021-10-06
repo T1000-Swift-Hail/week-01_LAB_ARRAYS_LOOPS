8c30114bfb630b7fa64a1ad893884f813ff27120
user <user@MacBook-Air-.local>
2021-10-06 10:19:51 +0300

    complete homework

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var Cities = ["Hail", "Dammam", "Jeddah"]
+
+Cities.append("Qaseem")
+Cities.append("Abha")
+Cities.insert("Riyadh", at:1)
+for visited in Cities {
+    print("i visited ", visited)
+    
+}
+        
+    
+
+
+
