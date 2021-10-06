cb90cef4ba9cbeb627ad5f9958b2b67b5a6fe549
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-06 10:12:28 +0300

    homework is done

---
+++ Array .playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var cities = ["KW"]
+
+cities.append("Hail")
+cities.append("Mikk")
+cities.append("Riyadh")
+cities.append("Buh")
+
+cities.insert("Tabok", at: 3)
+
+//cities += ["Hail","Mikk","Riyadh","Buh"]
+
+for (index,cit) in cities.enumerated(){
+    print ("\(index):\(cit)")
+}
+
+print(cities)
