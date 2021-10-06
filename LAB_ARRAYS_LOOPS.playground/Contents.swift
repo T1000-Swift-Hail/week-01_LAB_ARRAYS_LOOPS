5ed20aa420b904148dc81b9ea505915646013b7b
Njoud Alrshidi <njoudalrshidi@MacBook-Pro-Njoud.local>
2021-10-06 10:21:23 +0300

    LAB IS DONE

---
+++ LAB_ARRAYS_LOOPS.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var cities = ["Riyadh", "Makkah", "taef", "jeedah", "Abha"]
+
+
+cities.insert("tabuk", at: 3)
+for city in cities {
+    print("I visited \(city) ")
+    
+}
+
+
+
