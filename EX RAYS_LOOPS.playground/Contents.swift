cbb01a03ba22e9844f4243580554fc96f913256e
طلال عبيدالله دعيع القلادي <talalalglaadi@MacBook-Air-tlal.local>
2021-10-06 11:08:46 +0300

    EX Arrays Loop Done

---
+++ EX RAYS_LOOPS.playground/Contents.swift
@@ -0,0 +1,12 @@
+var cities = ["Makkah","Jeddah","Qassiam","Hail","Tabuk"]
+
+var visitedcities = ["Jeddah","Qassiam"]
+
+visitedcities += ["Tabuk"]
+
+for visted in visitedcities{
+    print(visted)
+}
+
+
+
